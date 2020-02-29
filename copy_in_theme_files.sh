dec604300526cf99a0995faef47e78f0459b0884
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-29 21:20:34 +0000

    Preserve file attributes when copying

--- copy_in_theme_files.sh
+++ copy_in_theme_files.sh
@@ -2,5 +2,5 @@ inputs="_data _includes _layouts _sass assets
 _config.yml Gemfile LICENSE minimal-mistakes-jekyll.gemspec package-lock.json package.json Rakefile"
 for dir in $inputs; do
   echo $dir
-    cp -r ../minimal-mistakes/$dir .
+    cp -p -r ../minimal-mistakes/$dir .
 done
