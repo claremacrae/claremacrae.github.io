bf010c4b67ada5697067837bc1ccdee6a5474448
Clare Macrae <github@cfmacrae.fastmail.co.uk>
2020-02-29 21:22:13 +0000

    Make script more robust

--- copy_in_theme_files.sh
+++ copy_in_theme_files.sh
@@ -1,3 +1,9 @@
+#!/bin/sh
+
+# Force execution to halt if there are any errors in this script:
+set -e
+set -o pipefail
+
 inputs="_data _includes _layouts _sass assets
 _config.yml Gemfile LICENSE minimal-mistakes-jekyll.gemspec package-lock.json package.json Rakefile"
 for dir in $inputs; do
